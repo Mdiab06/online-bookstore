834121d1db472f1daba6b4a4a391d3c79e10195d
Mdiab06 <mkdiab5106@gmail.com>
2020-05-17 17:20:02 -0500

    Intial Commit. Created a SpringBoot Backend App and the configuration for the application properties.

---
+++ onlinebookstore/onlinebookstore/src/main/java/diab/com/onlinebookstore/OnlinebookstoreApplication.java
@@ -0,0 +1,14 @@
+package diab.com.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookstoreApplication.class, args);
+		System.out.println("ALL DONE");
+	}
+
+}
