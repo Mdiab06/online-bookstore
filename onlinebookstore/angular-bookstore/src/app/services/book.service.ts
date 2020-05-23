416d36229cdb1d6fbd17f9a6e13c3a93e40bee19
Mdiab06 <mkdiab5106@gmail.com>
2020-05-23 00:04:23 -0500

    Feature 4 completed categories added to be distinguished in the FrontEnd

--- onlinebookstore/angular-bookstore/src/app/services/book.service.ts
+++ onlinebookstore/angular-bookstore/src/app/services/book.service.ts
@@ -3,12 +3,14 @@ import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Book } from '../common/book';
+import { BookCategory } from '../common/book-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
   private baseUrl = "http://localhost:8081/api/v1/books";
+  private categoryUrl = "http://localhost:8081/api/v1/book-category";
 
   constructor( private HttpClient: HttpClient) { }
 
@@ -19,10 +21,23 @@ export class BookService {
       map(response => response._embedded.books)
     );
   }
+
+  getBookCategories() : Observable<BookCategory[]>{
+    return this.HttpClient.get<getResponseBookCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.bookCategory)
+    );
+  }
 }
 
 interface getResponseBooks{
   _embedded:{
     books : Book[];
+  }
+}
+
+
+interface getResponseBookCategory{
+  _embedded:{
+    bookCategory : BookCategory[];
   }
 }
--- onlinebookstore/onlinebookstore/src/main/java/diab/com/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/onlinebookstore/src/main/java/diab/com/onlinebookstore/repository/BookCategoryRepository.java
@@ -2,6 +2,7 @@ package diab.com.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import diab.com.onlinebookstore.entity.BookCategory;
 
